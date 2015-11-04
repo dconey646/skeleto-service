8ceccb163854a48873bbfbf8cbe46da318cad55d
Simon Watson <S.Watson@kainos.com>
2015-11-04 13:43:10 +0000

    Implementing login functionality.

--- src/main/java/com/kainos/training/login/service/resource/LoginResource.java
+++ src/main/java/com/kainos/training/login/service/resource/LoginResource.java
@@ -1,28 +1,33 @@
 package com.kainos.training.login.service.resource;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Path("/login")
 @Produces(MediaType.APPLICATION_JSON)
 public class LoginResource {
-    private final String requiredUsername;
-    private final String requiredPassword;
+	private final String requiredUsername;
+	private final String requiredPassword;
 
-    public LoginResource(String requiredUsername, String requiredPassword) {
-        this.requiredUsername = requiredUsername;
-        this.requiredPassword = requiredPassword;
-    }
+	public LoginResource(String requiredUsername, String requiredPassword) {
+		this.requiredUsername = requiredUsername;
+		this.requiredPassword = requiredPassword;
+	}
 
-    @POST
-    public void login(@FormParam("username") String username,
-           @FormParam("password") String password) {
-    }
+	@POST
+	public Response login(@FormParam("username") String username, @FormParam("password") String password) {
+
+		if (username != null && username.equalsIgnoreCase(requiredUsername)) {
+			if (password != null && password.equals(requiredPassword)) {
+				return Response.status(Response.Status.NO_CONTENT).build();
+			}
+			return Response.status(Response.Status.UNAUTHORIZED).build();
+		}
+
+		return Response.status(Response.Status.NOT_FOUND).build();
+	}
 }
