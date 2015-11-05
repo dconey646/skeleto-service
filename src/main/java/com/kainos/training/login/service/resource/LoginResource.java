d22341fd854c0327ac03cbc316522899b086efa8
Simon Watson <S.Watson@kainos.com>
2015-11-05 14:45:52 +0000

    Updating service to accept JSON.

--- src/main/java/com/kainos/training/login/service/resource/LoginResource.java
+++ src/main/java/com/kainos/training/login/service/resource/LoginResource.java
@@ -1,14 +1,17 @@
 package com.kainos.training.login.service.resource;
 
 import javax.ws.rs.FormParam;
-import javax.ws.rs.POST;
+import javax.ws.rs.*;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.kainos.training.login.service.model.User;
+
 @Path("/login")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class LoginResource {
 	private final String requiredUsername;
 	private final String requiredPassword;
@@ -18,11 +21,24 @@ public class LoginResource {
 		this.requiredPassword = requiredPassword;
 	}
 
-	@POST
+	/* @POST
 	public Response login(@FormParam("username") String username, @FormParam("password") String password) {
 
 		if (username != null && username.equalsIgnoreCase(requiredUsername)) {
 			if (password != null && password.equals(requiredPassword)) {
+				return Response.status(Response.Status.NO_CONTENT).build();
+			}
+			return Response.status(Response.Status.UNAUTHORIZED).build();
+		}
+
+		return Response.status(Response.Status.NOT_FOUND).build();
+	} */
+	
+	@POST
+	public Response login(User user) {
+
+		if (user.getUsername() != null && user.getUsername().equalsIgnoreCase(requiredUsername)) {
+			if (user.getPassword() != null && user.getPassword().equals(requiredPassword)) {
 				return Response.status(Response.Status.NO_CONTENT).build();
 			}
 			return Response.status(Response.Status.UNAUTHORIZED).build();
