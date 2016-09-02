b70e74747730615a87e973d8742cfac5821be1a5
David Spence <D.Spence@kainos.com>
2016-09-02 09:31:02 +0100

    Updating service with example response and example test

--- src/main/java/com/kainos/training/login/service/resource/LoginResource.java
+++ src/main/java/com/kainos/training/login/service/resource/LoginResource.java
@@ -1,18 +1,18 @@
 package com.kainos.training.login.service.resource;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Path("/login")
 @Produces(MediaType.APPLICATION_JSON)
 public class LoginResource {
+
     private final String requiredUsername;
     private final String requiredPassword;
 
@@ -22,7 +22,14 @@ public class LoginResource {
     }
 
     @POST
-    public void login(@FormParam("username") String username,
+    public Response login(@FormParam("username") String username,
            @FormParam("password") String password) {
+
+        // todo: username and password check
+
+        return Response
+                .status(Response.Status.OK)
+                .entity("Success!")
+                .build();
     }
 }
